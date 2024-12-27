234171065be447386e17aa40c1ca463cbe99b0e7
cjohnson <collin.g.johnson@gmail.com>
2024-12-26 23:54:06 -0500

    Add Shift18

---
+++ 1-circuits/1-sequential-logic/2-shift-registers/2-shift18/top_module.sv
@@ -0,0 +1,28 @@
+// https://hdlbits.01xz.net/wiki/Shift18
+
+module top_module (
+    input clk,
+    input load,
+    input ena,
+    input [1:0] amount,
+    input [63:0] data,
+    output reg [63:0] q
+);
+  always @(posedge clk) begin
+    if (load) q <= data;
+    else if (ena) begin
+      unique case (amount)
+        2'b00: q <= q << 1;
+        2'b01: q <= q << 8;
+        2'b10: begin
+          q <= q >> 1;
+          q[63] <= q[63];
+        end
+        2'b11: begin
+          q <= q >> 8;
+          q[63:56] <= {8{q[63]}};
+        end
+      endcase
+    end
+  end
+endmodule
